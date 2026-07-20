352623ac4986646eaf5cab6be63696ef23518228
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-07-20 12:58:47 +0530

    Fix Express 5 wildcard routing error

--- backend/server.js
+++ backend/server.js
@@ -330,7 +330,7 @@ const seedDatabase = async () => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 
-  app.get("*",(req,res)=>{
+  app.get("*any",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   })
 
